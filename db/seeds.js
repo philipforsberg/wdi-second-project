0c1f865847a6819d873210edfc456643f040de22
Philip <philippernf@gmail.com>
2018-01-17 11:02:11 +0000

    creating users in seeds and trying to make comments get ids

--- db/seeds.js
+++ db/seeds.js
@@ -3,3 +3,9 @@ const { dbURI } = require('../config/environment');
 
 mongoose.Promise = require('bluebird');
 mongoose.connect(dbURI);
+
+const List = require('../models/list');
+const User = require('../models/user');
+
+List.collection.drop();
+User.collection.drop();
